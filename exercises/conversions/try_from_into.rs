cb9b0615449d6dd1e99a6763ca108e2e46f0663c
Stef Robbe <stef.robbe@protonmail.com>
2024-06-30 19:57:59 +0200

    feat: 100% done

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,21 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn to_u8(value: i16) -> Result<u8, IntoColorError> {
+    u8::try_from(value).map_err(|_| IntoColorError::IntConversion)
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+
+        let red = to_u8(red)?;
+        let green = to_u8(green)?;
+        let blue = to_u8(blue)?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -48,6 +57,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = to_u8(arr[0])?;
+        let green = to_u8(arr[1])?;
+        let blue = to_u8(arr[2])?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -55,6 +69,15 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let red = to_u8(slice[0])?;
+        let green = to_u8(slice[1])?;
+        let blue = to_u8(slice[2])?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
